43106a1424e09a7f15f688da0ae39f9103439659
Avi Brown <avinbrown.focus@icloud.com>
2023-02-16 23:54:27 -0500

    changed from a switch statment to a map hash table for more concise code.

--- helloworld.go
+++ helloworld.go
@@ -8,16 +8,18 @@ func main() {
    greeting := greet("heb")
    fmt.Println(greeting)
 }
-
+var phrasebook = map[language]string{
+    "el": "Χαίρετε Κόσμε", // Greek
+    "en": "Hello World",    // English
+    "fr": "Bonjor le monde", // French
+    "he": "שלום עולם",      // Hebrew
+    "ur": "ہیلو دنیا",          // Urdu
+    "vi": "Xin chào Thế Giới", // Vietnamese
+}
 func greet(l language) string {
-    switch l  {
-    case "en":
-        return "Hello World"
-    case "fr":
-        return "Bonjour le monde"
-    case "heb":
-        return "Shalom Olam"
-    default:
-        return ""
+    greeting, ok := phrasebook[l]
+    if !ok {
+        return fmt.Sprintf("unsupported langiage: %q", l)
     }
+    return greeting
 }
