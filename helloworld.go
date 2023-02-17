3071f9aa0ed3a51d1a7fe0707e18fa038b363c59
Avi Brown <avinbrown.focus@icloud.com>
2023-02-17 00:05:38 -0500

    added go fmt

--- helloworld.go
+++ helloworld.go
@@ -5,21 +5,24 @@ import "fmt"
 type language string
 
 func main() {
-   greeting := greet("heb")
-   fmt.Println(greeting)
+	greeting := greet("heb")
+	fmt.Println(greeting)
 }
+
 var phrasebook = map[language]string{
-    "el": "Χαίρετε Κόσμε", // Greek
-    "en": "Hello World",    // English
-    "fr": "Bonjor le monde", // French
-    "he": "שלום עולם",      // Hebrew
-    "ur": "ہیلو دنیا",          // Urdu
-    "vi": "Xin chào Thế Giới", // Vietnamese
+	"el": "Χαίρετε Κόσμε",     // Greek
+	"en": "Hello World",       // English
+	"fr": "Bonjor le monde",   // French
+	"he": "שלום עולם",         // Hebrew
+	"ur": "ہیلو دنیا",         // Urdu
+	"vi": "Xin chào Thế Giới", // Vietnamese
 }
+
 func greet(l language) string {
-    greeting, ok := phrasebook[l]
-    if !ok {
-        return fmt.Sprintf("unsupported langiage: %q", l)
-    }
-    return greeting
+	// value // bool
+	greeting, ok := phrasebook[l]
+	if !ok {
+		return fmt.Sprintf("unsupported langiage: %q", l)
+	}
+	return greeting
 }
--- helloworld_internal_test.go
+++ helloworld_internal_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
 //func ExampleMain(){
@@ -11,30 +11,30 @@ import (
 //}
 
 func TestGreet_English(t *testing.T) {
-    lang := "en"
-    expectedGreeting := "Hello World"
-    greeting := greet(language(lang))
-    if greeting != expectedGreeting {
-        // mark this a failed
-        t.Errorf("expected %q, got: %q", expectedGreeting, greeting)
-    }
+	lang := "en"
+	expectedGreeting := "Hello World"
+	greeting := greet(language(lang))
+	if greeting != expectedGreeting {
+		// mark this a failed
+		t.Errorf("expected %q, got: %q", expectedGreeting, greeting)
+	}
 }
 func TestGreet_French(t *testing.T) {
-    lang := "fr"
-    expectedGreeting := "Bonjour le monde"
-    greeting := greet(language(lang))
+	lang := "fr"
+	expectedGreeting := "Bonjour le monde"
+	greeting := greet(language(lang))
 
-    if expectedGreeting != greeting {
-        t.Errorf("expected: %q, got: %q", expectedGreeting, greeting)
-    }
+	if expectedGreeting != greeting {
+		t.Errorf("expected: %q, got: %q", expectedGreeting, greeting)
+	}
 }
 
-func TestGreeting_Hebrew(t *testing.T)  {
-    lang := "heb"
-    expectedGreeting := "Shalom Olam"
-    greeting := greet(language(lang))
+func TestGreeting_Hebrew(t *testing.T) {
+	lang := "heb"
+	expectedGreeting := "Shalom Olam"
+	greeting := greet(language(lang))
 
-    if expectedGreeting != greeting {
-        t.Errorf("expected: %q, got: %q", expectedGreeting, greeting)
-    }
+	if expectedGreeting != greeting {
+		t.Errorf("expected: %q, got: %q", expectedGreeting, greeting)
+	}
 }
